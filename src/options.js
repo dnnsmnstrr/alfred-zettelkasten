12f69d1d3a53b3e69dad15d39285f5f06a7b3459
Dennis Muensterer <dennismuensterer@gmail.com>
2021-09-06 02:03:42 +0200

    Move web into generic options generation

--- src/options.js
+++ src/options.js
@@ -1,19 +1,31 @@
 const {getAppIcon, getIcon} = require('./icons');
-const { BASE_URL } = process.env
+const {BASE_URL, DEPLOYMENT_PATH} = process.env; // environment variables
+const PROJECT_PATH = '/' + DEPLOYMENT_PATH.split('/')[1]
 
 const search = require('./search');
 const newNote = require('./newNote');
 
-const makeOption = ({ name, subtitle, autocomplete, icon, iconPath, mods }) => {
-  return {
-    arg: name.toLowerCase(),
-    title: name,
-    subtitle: subtitle || 'Open Zettelkasten in ' + name,
-    autocomplete: autocomplete || name.toLowerCase(),
-    icon: icon || getAppIcon(name, iconPath),
-    mods
+const makeMods = (options = []) => {
+  if (!Array.isArray(options)) {
+    // default to command for single option
+    options = [options]
   }
+  const modOrder = ['cmd', 'alt', 'ctrl', 'fn', 'shift']
+  const mods = {}
+  options.forEach((option, i) => {
+    mods[modOrder[i]] = option
+  });
+  return mods
 }
+
+const makeOption = ({ name, subtitle, autocomplete, icon, iconPath, mods }) => ({
+  arg: name.toLowerCase(),
+  title: name,
+  subtitle: subtitle || 'Open Zettelkasten in ' + name,
+  autocomplete: autocomplete || name.toLowerCase(),
+  icon: icon || getAppIcon(name, iconPath),
+  mods: makeMods(mods)
+})
 
 const options = [
   {
@@ -23,27 +35,29 @@ const options = [
   {
     name: 'Atom',
     mods: {
-      cmd: {
-        arg: 'search/atom',
-        subtitle: 'Toggle search in Atom'
-      }
+      arg: 'search/atom',
+      subtitle: 'Toggle search in Atom'
     }
   },
   {
     name: 'Web',
     subtitle: 'Go to the deployed Zettelkasten',
     icon: getIcon('GenericNetworkIcon'),
-    mods: {
-      cmd: {
-        arg: 'https://www.cerveau.app/-/dnnsmnstrr/zettelkasten',
-        subtitle: 'Edit in Cerveau'
+    mods: [
+      {
+        arg: BASE_URL.replace('github', 'gitlab') + PROJECT_PATH,
+        subtitle: 'Open dendron version'
       },
-      alt: {
-        arg: 'https://github.com/dnnsmnstrr/zettelkasten',
+      {
+        arg: 'https://github.com/' + DEPLOYMENT_PATH,
         subtitle: 'Open in GitHub',
         icon: getAppIcon('GitHub Desktop')
-      }
-    }
+      },
+      {
+        arg: 'https://www.cerveau.app/-/' + DEPLOYMENT_PATH,
+        subtitle: 'Edit in Cerveau'
+      },
+    ]
   }
 ].map(makeOption)
 
